df55c8d103f80eb2cd7ec765cacc2dd7746f5d9e
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-08-03 14:54:33 +0530

    4323431454030824 commit

---
+++ src/test/java/Pkg/Extent/DummyCustomListener.java
@@ -0,0 +1,15 @@
+package Pkg.Extent;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class DummyCustomListener implements ITestListener{
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("test success");
+	}
+	@Override
+	public void onTestFailure(ITestResult result) {
+		System.out.println("test failure");
+	}
+}
